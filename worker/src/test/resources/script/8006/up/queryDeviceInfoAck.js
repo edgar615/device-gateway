7833a42ee7d372a7ac75823019313dc71e8cee96
edgar615 <edgar615@gmail.com>
2018-07-16 18:22:53 +0800

    脚本

--- worker/src/test/resources/script/8006/up/queryDeviceInfoAck.js
+++ worker/src/test/resources/script/8006/up/queryDeviceInfoAck.js
@@ -10,8 +10,8 @@ function execute(input, logger) {
     report.data = new Map();
     list.add(report);
 
-    for (var i = 0; i < input.checksums.length; i++) {
-        var checksumData = input.checksums[i];
+    for (var i = 0; i < input.data.checksums.length; i++) {
+        var checksumData = input.data.checksums[i];
         //0所有分区，1无线探测器，2有线防区，3无线遥控器，4 无线警号，5 有线警号，6 RFID, 7无线PGM ,8 有线PGM
         var deviceType = checksumData.deviceType;
         var result = checksumData.result;
