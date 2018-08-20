766fec43712966b162a5273d16ae5bb883fad72a
edgar615 <edgar615@gmail.com>
2018-08-20 11:28:15 +0800

    修改心跳BUG

--- worker/src/main/java/com/github/edgar615/device/gateway/worker/WorkerVerticle.java
+++ worker/src/main/java/com/github/edgar615/device/gateway/worker/WorkerVerticle.java
@@ -38,53 +38,7 @@ public class WorkerVerticle extends AbstractVerticle {
   @Override
   public void start(Future<Void> startFuture) throws Exception {
     deviceMessageHandler = new DeviceMessageHandler(vertx);
-    //接受设备上下线的消息，入队
-    vertx.eventBus().<JsonObject>consumer(DEFAULT_FIRST_CONN_ADDRESS, msg -> {
-      JsonObject jsonObject = msg.body();
-      //包装为event
-      String id = jsonObject.getString("id");
-      List<String> idSplitter = Splitter.on(":").splitToList(id);
-      String deviceIdentifier = idSplitter.get(1);
-      String productType = idSplitter.get(0);
-      Long time = jsonObject.getLong("time", Instant.now().getEpochSecond());
-      Map<String, Object> brokerMessage = new HashMap<>();
-      brokerMessage.put("productType", productType);
-      brokerMessage.put("topic", "__inner");
-      brokerMessage.put("deviceIdentifier", deviceIdentifier);
-      brokerMessage.put("traceId", UUID.randomUUID().toString());
-      brokerMessage.put("command", KeepaliveCommand.CONNECT);
-      brokerMessage.put("data", new HashMap<>());
-      brokerMessage.put("type", MessageType.KEEPALIVE);
-      deviceMessageHandler.handle(brokerMessage, ar -> {
-        //do nothing
-      });
-    });
-
-    //掉线
-    vertx.eventBus().<JsonObject>consumer(DEFAULT_DISCONN_ADDRESS, msg -> {
-      JsonObject jsonObject = msg.body();
-      JsonArray ids = jsonObject.getJsonArray("ids", new JsonArray());
-      Long time = jsonObject.getLong("time", Instant.now().getEpochSecond());
-      for (int i = 0; i < ids.size(); i++) {
-        String id = ids.getString(i);
-        //包装为event
-        List<String> idSplitter = Splitter.on(":").splitToList(id);
-        String deviceIdentifier = idSplitter.get(1);
-        String productType = idSplitter.get(0);
-        Map<String, Object> brokerMessage = new HashMap<>();
-        brokerMessage.put("productType", productType);
-        brokerMessage.put("topic", "__inner");
-        brokerMessage.put("deviceIdentifier", deviceIdentifier);
-        brokerMessage.put("traceId", UUID.randomUUID().toString());
-        brokerMessage.put("command", KeepaliveCommand.DIS_CONNECT);
-        brokerMessage.put("data", new HashMap<>());
-        brokerMessage.put("type", MessageType.KEEPALIVE);
-        deviceMessageHandler.handle(brokerMessage, ar -> {
-          //do nothing
-        });
-      }
-    });
-
+  
     //kafka的消息
     vertx.eventBus().<JsonObject>consumer(Consts.LOCAL_EVENT_HANDLER, msg -> {
       try {
